5fcd6e2178a186cfa20ea3540480dcac57234be1
VISWESWARA-JEERU <jvsks28@gmail.com>
2025-11-24 19:43:08 +0530

    comments added

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ function App() {
    
 const [length, setLength] = useState(0);
 const [randomString, setRandomString] = useState('');
-console.log(length);
+// console.log(length);
 
-const generateRandomString = () => {
+const generateRandomString = () => { // function to generate random string
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+`-={}[]|;:"<>,.?/';
   let result = '';
   const charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for ( let i = 0; i < length; i++ ) { // loop to create string of desired length
+    result += characters.charAt(Math.floor(Math.random() * charactersLength)); // append random character to result
   }
   setRandomString(result);
 }
-  return (
+  return ( // main return
     <div className='bg-blue-500  w-screen h-screen flex justify-center items-center'>
       <div className='bg-blue-200 h-40 w-100 rounded-lg flex  justify-center items-center flex-col gap-y-5'>
-        <h3 className='font-semibold text-white'>Random String Generator </h3>
+        <h3 className='font-semibold text-white'>Random String Generator </h3> 
         <div className='flex justify-center items-center gap-x-6 '>
           <input type="number" min="1" className='bg-white rounded-lg h-9 outline-none px-2 text-center'onChange={(e)=>setLength(e.target.value)} />
           <button className='p-1.5 bg-blue-500 text-white rounded-lg'onClick={generateRandomString}>Click</button>
